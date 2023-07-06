91cf99ca08cc80bcb54d5c46f3cf9308fa12d815
Hanne Sandbakken <hsandbakken94@gmail.com>
2023-07-06 11:27:42 +0200

    seeded data category and words

--- dictionary/Data/DictionaryDbContext.cs
+++ dictionary/Data/DictionaryDbContext.cs
@@ -13,52 +13,68 @@ namespace dictionary.Data
         public DbSet<Project> Projects { get; set; }
         public DbSet<WordProject> WordsProjects { get; set; }
 
-        //protected override void OnModelCreating(ModelBuilder modelBuilder)
-        //{
-        //    base.OnModelCreating(modelBuilder);
-        //    modelBuilder.Entity<Word>().HasData(
-        //        new Word
-        //        {
-        //            Id = 1,
-        //            NorwegianWord = "hus",
-        //            GermanWord = "Haus"
-        //        },new Word
-        //        {
-        //            Id = 2,
-        //            NorwegianWord = "flaske",
-        //            GermanWord = "Flasche"
-        //        },new Word
-        //        {
-        //            Id = 3,
-        //            NorwegianWord = "gå",
-        //            GermanWord = "gehen"
-        //        },new Word
-        //        {
-        //            Id = 4,
-        //            NorwegianWord = "sitte",
-        //            GermanWord = "sitzen"
-        //        },new Word
-        //        {
-        //            Id = 5,
-        //            NorwegianWord = "blå",
-        //            GermanWord = "blau"
-        //        },new Word
-        //        {
-        //            Id = 6,
-        //            NorwegianWord = "liten",
-        //            GermanWord = "Klein"
-        //        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Category>().HasData(
+               new Category
+               {
+                   Id = 1,
+                   Name = "Noun",
+               }, new Category
+               {
+                   Id = 2,
+                   Name = "Verb",
+               }, new Category
+               {
+                   Id = 3,
+                   Name = "Adjective",
+               }
+           );
+            modelBuilder.Entity<Word>().HasData(
+                new Word
+                {
+                    Id = 1,
+                    NorwegianWord = "hus",
+                    GermanWord = "Haus",
+                    CategoryId = 1
+                }, new Word
+                {
+                    Id = 2,
+                    NorwegianWord = "flaske",
+                    GermanWord = "Flasche",
+                    CategoryId = 1
+       
+                }, new Word
+                {
+                    Id = 3,
+                    NorwegianWord = "gå",
+                    GermanWord = "gehen",
+                    CategoryId = 2
+                }, new Word
+                {
+                    Id = 4,
+                    NorwegianWord = "sitte",
+                    GermanWord = "sitzen",
+                    CategoryId = 2
+                }, new Word
+                {
+                    Id = 5,
+                    NorwegianWord = "blå",
+                    GermanWord = "blau",
+                    CategoryId = 3
+                }, new Word
+                {
+                    Id = 6,
+                    NorwegianWord = "liten",
+                    GermanWord = "Klein",
+                    CategoryId = 3
+                }
 
-        //    );
-        //    modelBuilder.Entity<Category>().HasData(
-        //        new Category
-        //        {
-        //            Id = 1,
+            );
+           
 
-        //        }
-        //    );
-            
-        //}
+        }
 
     }
 }
--- dictionary/Migrations/20230706092411_SeededCategoiesyAndWords.Designer.cs
+++ dictionary/Migrations/20230706092411_SeededCategoiesyAndWords.Designer.cs
@@ -2,6 +2,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using dictionary.Data;
 
@@ -10,9 +11,11 @@ using dictionary.Data;
 namespace dictionary.Migrations
 {
     [DbContext(typeof(DictionaryDbContext))]
-    partial class DictionaryDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230706092411_SeededCategoiesyAndWords")]
+    partial class SeededCategoiesyAndWords
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
