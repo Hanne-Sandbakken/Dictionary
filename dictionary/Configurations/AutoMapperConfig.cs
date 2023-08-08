74af0a30095da6acb542e8f6fa1019c55d9ad6ba
Hanne Sandbakken <hsandbakken94@gmail.com>
2023-08-08 17:51:50 +0200

    AutoMapper for Word, WordProject and Project

--- dictionary/Configurations/AutoMapperConfig.cs
+++ dictionary/Configurations/AutoMapperConfig.cs
@@ -1,7 +1,9 @@
 ﻿using AutoMapper;
 using dictionary.Data;
 using dictionary.Dto.Category;
+using dictionary.Dto.Project;
 using dictionary.Dto.Word;
+using dictionary.Dto.WordProjects;
 
 namespace dictionary.Configurations
 {
@@ -9,13 +11,36 @@ namespace dictionary.Configurations
     {
         public AutoMapperConfig()
         {
+
+            //Category:
             CreateMap<Category, CreateCategoryDto>().ReverseMap();
             CreateMap<Category, GetCategoryDto>().ReverseMap();
             CreateMap<Category, GetCategoryDetailsDto>().ReverseMap();
             CreateMap<Category, UpdateCategoryDto>().ReverseMap();
 
+            //Word:
             CreateMap<Word, GetWordDto>().ReverseMap();
-           
+            CreateMap<Word, GetWordDetailsDto>().ReverseMap();
+            CreateMap<Word, CreateWordDto>().ReverseMap();
+            CreateMap<Word, UpdateWordDto>().ReverseMap();
+
+            //WordProject:
+            CreateMap<WordProject, GetWordProjectDto>().ReverseMap();
+            CreateMap<WordProject, GetWordProjectDetailsDto>().ReverseMap();
+            CreateMap<WordProject, CreateWordProjectDto>().ReverseMap();
+            CreateMap<WordProject, UpdateWordDto>().ReverseMap();
+
+            //Project:
+            CreateMap<Project, GetProjectDto>().ReverseMap();
+            CreateMap<Project, GetProjectDetailsDto>().ReverseMap();
+            CreateMap<Project, CreateProjectDto>().ReverseMap();
+            CreateMap<Project, UpdateProjectDto>().ReverseMap();
+
+
+
+
+
+
         }
     }
 }
